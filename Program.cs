fe51ea11d1c150018347e01e0a694bbb1a9458c9
Dmitry <greendima13@mail.ru>
2022-12-06 01:01:27 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -36,3 +36,18 @@
 // {
 //     Console.WriteLine($"{numb3} большее число");
 // }
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// if (numb % 2 == 0)
+// {
+//     Console.WriteLine($"Число {numb} четное");
+// }
+// else 
+// {
+//     Console.WriteLine($"Число {numb} нечетное");
+// }
