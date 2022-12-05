2e0095f5361e773b4c6a55db5258758c51285cbc
Dmitry <greendima13@mail.ru>
2022-12-06 00:35:27 +0300

    Task 2

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите два чиcла");
+// Console.Write("Первое ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Второе ");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+// if (numb1 > numb2)
+// {
+//     Console.WriteLine($"{numb1} большее число");
+// }
+// else 
+// {
+//     Console.WriteLine($"{numb2} большее число");
+// }
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите два чиcла");
+// Console.Write("Первое ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Второе ");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Третье ");
+// int numb3 = Convert.ToInt32(Console.ReadLine());
+// if (numb1 > numb2)
+// {
+//     Console.WriteLine($"{numb1} большее число");
+// }
+// if (numb2 > numb3)
+// {
+//     Console.WriteLine($"{numb2} большее число");
+// }
+// else 
+// {
+//     Console.WriteLine($"{numb3} большее число");
+// }
